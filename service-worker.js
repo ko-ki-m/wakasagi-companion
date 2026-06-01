30e2017e947261ad9cb5fa4b8f8d655e21a8bbcc
ko-ki-m <cozy_m1633@jcom.zaq.ne.jp>
2026-06-01 10:24:04 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,4 @@
 // Wakasagi Map rollback v11.5 - service worker disabled/unregister
 self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => {
-  event.waitUntil((async()=>{
-    try{
-      const keys = await caches.keys();
-      await Promise.all(keys.map(k => caches.delete(k)));
-      await self.registration.unregister();
-    }catch(e){}
-  })());
-});
+self.addEventListener('activate', event => { event.waitUntil((async()=>{ try{ const keys = await caches.keys(); await Promise.all(keys.map(k => caches.delete(k))); await self.registration.unregister(); }catch(e){} })()); });
 self.addEventListener('fetch', event => {});
